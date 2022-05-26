639737b4a5ae82b842243f22a037f4e20641b429
ShinJaeIl01 <ekvm555@naver.com>
2022-05-26 15:17:32 +0900

    template

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+// Options API
+export default {
+  data() {
+    return {
+      msg: 'Hello'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '~', replacement: `${__dirname}/src`} // find 경로 별칭, replacement 설정할 경로
+    ]
+  }
+})
